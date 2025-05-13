b19d1932a351f0d8ebe29a6fc3022bdd651e320c
Eric Romano <eric.romano@intelsat.com>
2025-05-13 15:07:36 -0400

    Changed startup behavior: if it fails to connect to mongodb, it exits.

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,6 @@
 """Dependencies for FastAPI startup"""
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from pymongo.errors import ServerSelectionTimeoutError
 from fastapi import FastAPI, Request
 from app.settings import settings
 
@@ -10,7 +11,12 @@ print("DB_NAME:", settings.DB_NAME)
 
 async def connect_to_mongo(app: FastAPI) -> None:
     """Establishes connection to Mongo"""
-    app.state.mongo_client = AsyncIOMotorClient(settings.MONGO_URI)
+    try:
+        app.state.mongo_client = AsyncIOMotorClient(settings.MONGO_URI, serverSelectionTimeoutMS=settings.MONGO_CONNECTION_TIMEOUT)
+        await app.state.mongo_client.admin.command('ping')
+
+    except ServerSelectionTimeoutError:
+        raise
 
 
 async def close_mongo_connection(app: FastAPI) -> None:
